bfebc0cfc4e3fe1ca3a74e62d291ca7d974f2498
sam <sailesh.18738@knit.ac.in>
2022-08-14 15:20:48 +0530

    Lecture1 finished

---
+++ L1.js
@@ -0,0 +1,8 @@
+console.log("JavaScript")
+let a=10;
+let b=20;
+c=a+b
+console.log(`Sum:`,c);
+const Name = "Sailesh kumar";
+// Name = "Shreya";//it will through error coz we can not re assign constant vrivables in JS
+console.log(Name)
